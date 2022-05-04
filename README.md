7040ea2593948bf5d652994416a38f227aa940cd
Carlos <carlos_alberto_lira@hotmail.com>
2022-05-03 20:56:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Almacén de productos📌
+
+\*\*Se busca crear un sistema para organizar los productos en un almacén. Se requiere poder dar de alta los productos y también listarlos y mostrar todos sus atributos.
+Todo producto contará con los siguientes atributos:
+• Nombre • SKU
+• Marca
+• Costo
+Se tendrán 3 categorías de productos y algunos atributos específicos:
+• Televisor
+o Tipo de pantalla (LED | LCD | OLED) o Tamaño de pantalla
+• Laptops
+o Procesador (Intel | AMD)
+o Memoria Ram • Zapatos
+o Material (Piel | Plástico) o Numero / Tamaño
+Se mostrará el precio de venta, aplicando los siguientes márgenes de utilidad: Televisor 35%, Laptops 40% y Zapatos 30%. Estos porcentajes estarán definidos en el código fuente..
---
+++ src/app.js
@@ -0,0 +1,34 @@
+// ************ Require's ************
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 3001;
+
+// ************ express() ************
+const app = express();
+
+// ************ Middlewares  ************
+// Configuración para la carpeta public
+app.use(express.static(path.resolve(__dirname, "../public")));
+//Captura la informacion que viene del formulario via post
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// ************ Template Engine - (don't touch) ************
+//Configuración para ejs y la carpeta view
+app.set("view engine", "ejs");
+//Configuración para las vistas que estan dentro de subcarpetas en view
+app.set("views", path.join(__dirname, "views"));
+
+// ************ Route System require and use() ************
+const mainRoutes = require("./routes/mainRoute");
+
+app.use("/", mainRoutes);
+
+app.get("/", function (req, res) {
+  res.send("Bienvenidos al sitio");
+});
+
+//Al final levantamos el servidor
+app.listen(port, () => {
+  console.log("Servidor corriendo en el puerto 3001");
+});
